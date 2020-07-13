cfb1810ddfeb9f7042b8fbcd5cd0b43cf06b6361
Júlia Paixão <julia_paixao@id.uff.br>
2020-07-13 00:24:18 -0300

    Implementação da classe Boleto

--- src/Boleto.java
+++ src/Boleto.java
@@ -1,4 +1,28 @@
 
 public class Boleto {
+	
+	String codigo;
+	String data;
+	double valorPago;
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public double getValorPago() {
+		return valorPago;
+	}
+	public void setValorPago(double valorPago) {
+		this.valorPago = valorPago;
+	}
 
 }
