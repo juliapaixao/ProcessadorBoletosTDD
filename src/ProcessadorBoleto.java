c54550092750ff9727422d12dc9dd471df9a15b6
Júlia Paixão <julia_paixao@id.uff.br>
2020-07-13 00:54:41 -0300

    Implementação da classe ProcessadorBoleto

--- src/ProcessadorBoleto.java
+++ src/ProcessadorBoleto.java
@@ -1,8 +1,20 @@
+import java.util.List;
 
 public class ProcessadorBoleto {
+	double somaBoleto;
 
-	public boolean processa(boolean b) {
-		return true;
+
+	public boolean processa(Fatura fatura, List<Boleto> listBoleto) {
+		
+		for (Boleto boleto : listBoleto) {
+			somaBoleto = somaBoleto + boleto.getValorPago();
+		}
+		
+		
+		if (fatura.getValorTotal() < somaBoleto) {
+			return true;
+		}
+		else
+			return false;
 	}
-
 }
