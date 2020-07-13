de86030a7a0aeecc161ed3944a4fdb29fa4dc22f
Júlia Paixão <julia_paixao@id.uff.br>
2020-07-12 23:20:07 -0300

    Criação da fatura na classe teste

--- test/ProcessadorBoletosTest.java
+++ test/ProcessadorBoletosTest.java
@@ -7,8 +7,11 @@ public class ProcessadorBoletosTest {
 	@Test
 	public void verificaPagamento1() {
 		ProcessadorBoleto p = new ProcessadorBoleto();
+		Fatura f = new Fatura();
+		
 		boolean esperado = true;
 		boolean atual = p.processa(true);
+		
 		assertTrue(atual);
 	}
 
