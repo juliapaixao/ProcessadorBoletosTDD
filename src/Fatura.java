fa0f31e4c3d273aa2d07c62427bdc5e58cea807e
Júlia Paixão <julia_paixao@id.uff.br>
2020-07-13 00:22:01 -0300

    implementação da classe Fatura

--- src/Fatura.java
+++ src/Fatura.java
@@ -1,4 +1,30 @@
 
 public class Fatura {
+	
+	String data;
+	double valorTotal;
+	String cliente;
+	
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public String getCliente() {
+		return cliente;
+	}
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	
 
 }
